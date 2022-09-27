e7ba8fb41796e3d3335df04c19fef9dd5ff3d173
amvkrish <110442425+amvkrish@users.noreply.github.com>
2022-09-28 00:35:56 +0530

    my changes

---
+++ src/Stringmethods.java
@@ -0,0 +1,20 @@
+
+public class Stringmethods {
+
+	public static void main(String[] args) {
+		
+		String name = "MuRaLi KrIsHnA";
+		
+		System.out.println("Name:" + name);
+		
+		System.out.println("upper case :" + name.toUpperCase());
+		System.out.println("lower case:" + name.toLowerCase());
+		System.out.println("first char:" + name.charAt(0));
+		System.out.println("length:" + name.length());
+		System.out.println("last char:" + name.charAt(13));
+		System.out.println("substring:" + name.substring(7, 14));
+		System.out.println("concat:" + name.concat(name));
+	}
+
+	
+}
